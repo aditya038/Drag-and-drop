670a736b2742eb3894ff1a6513a8a6e97ef5675e
Aditya <adityachauhan0607@gmail.com>
2025-04-11 20:32:22 +0530

    feat:created temp components of heading, paragraph and button

---
+++ src/components/Heading.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Heading = () => {
+  
+  return (
+    <h1 class="mt-5">Heading</h1>
+  );
+};
---
+++ src/components/Paragraph.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Paragraph = () => {
+  
+  return (
+    <p className="mt-5">Paragraph</p>
+  )
+}
