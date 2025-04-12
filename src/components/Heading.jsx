083b90898c3dc438bac3a8f20071123b8f5b48ab
Aditya <adityachauhan0607@gmail.com>
2025-04-12 21:20:56 +0530

    fix:changed ui

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 
 export const Heading = ({ id, onDeleteComponent }) => {
   const headingRef = useRef(null);
-  const [text, setText] = useState("Edit me");
+  const [text, setText] = useState("Add Heading");
 
   useEffect(() => {
     if (headingRef.current) {
@@ -16,18 +16,18 @@ export const Heading = ({ id, onDeleteComponent }) => {
   };
 
   return (
-    <div className="flex border-2">
+    <div className="flex border-2 m-5 p-3">
       <h1
         ref={headingRef}
         contentEditable
         suppressContentEditableWarning
         onInput={handleInput}
-        className="text-3xl font-bold cursor-text outline-none mt-5 flex-1"
+        className="text-3xl font-bold cursor-text outline-none flex-1"
       />
       <button
         type="button"
         onClick={() => onDeleteComponent(id)}
-        className="text-white bg-red-700 hover:bg-red-800 font-large rounded-xl text-lg px-3 text-center"
+        className="text-white bg-red-700 hover:bg-red-800 font-large rounded-full text-lg px-3 text-center"
       >
         X
       </button>
