4831956cf6b0d604a246824cc7d70e90f10d1507
Aditya <adityachauhan0607@gmail.com>
2025-04-13 22:30:30 +0530

    fix:fixed issue of button inside button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,19 +2,19 @@ import React from "react";
 
 export const Button = ({ id, onDeleteComponent }) => {
   return (
-    <div className="flex m-5 p-1.5">
+    <div className="m-5 p-1.5">
       <button
         type="button"
         class="text-white bg-blue-700 hover:bg-blue-800 font-large rounded-lg text-lg px-5 py-2.5 me-2 mb-2"
       >
         Submit
-        <button
-          type="button"
-          onClick={() => onDeleteComponent(id)}
-          className="text-white bg-red-700 hover:bg-red-800 font-bold rounded-full text-lg px-2 text-center ml-4"
-        >
-          X
-        </button>
+      </button>
+      <button
+        type="button"
+        onClick={() => onDeleteComponent(id)}
+        className="text-white bg-red-700 hover:bg-red-800 font-bold rounded-full text-lg px-2 text-center ml-2"
+      >
+        X
       </button>
     </div>
   );
